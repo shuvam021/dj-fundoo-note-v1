5669a945cd677ead25fc869bae52431d77027786
shuvam021 <career.shuvam@gmail.com>
2022-03-10 09:58:52 +0530

    [Shuavm] Refactor update method of user view/serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,16 +1,11 @@
-from ast import mod
-from dataclasses import field
-
 from django.contrib.auth.models import User
-from pyexpat import model
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name',
-                  'last_name', 'email', 'password')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -19,9 +14,3 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-
-
-class UserUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
--- users/views.py
+++ users/views.py
@@ -6,25 +6,27 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 
-from users.serializers import UserSerializer, UserUpdateSerializer
+from users.serializers import UserSerializer
 from users.utils import ApiResponse
 
-logging.basicConfig(filename=settings.LOG_FILE,
-                    encoding='utf-8', level=logging.warning,
+logging.basicConfig(filename=settings.LOG_FILE, encoding='utf-8', level=logging.warning,
                     format='%(levelname)s - [%(asctime)s] - %(message)s')
-
 
 logger = logging.getLogger(__name__)
 
 
 # Create your views here.
 class UserApiView(APIView):
+    """User View class"""
+
     def get(self, request):
+        """List all users"""
         qs = User.objects.all()
         serializer = UserSerializer(qs, many=True)
         return ApiResponse(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
+        """Save new users"""
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -33,7 +35,12 @@ class UserApiView(APIView):
 
 
 class UserDetailsApiView(APIView):
-    def get_object(self, pk):
+    @staticmethod
+    def get_object(pk):
+        """
+        Return a user with matching pk value.
+        If pk value doesn't exist then raise a 404 not found error
+        """
         logger.info("User() accessed")
         try:
             return User.objects.get(pk=pk)
@@ -41,19 +48,22 @@ class UserDetailsApiView(APIView):
             raise Http404
 
     def get(self, request, pk):
+        """Return a user with matching pk value"""
         qs = self.get_object(pk)
         serializer = UserSerializer(qs)
         return ApiResponse(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
+        """Update the user's details with matching pk value"""
         qs = self.get_object(pk)
-        serializer = UserUpdateSerializer(qs, data=request.data)
+        serializer = UserSerializer(qs, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return ApiResponse(data=serializer.data, status=status.HTTP_201_CREATED)
         return ApiResponse(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
+        """Remove the user's details with matching pk value"""
         qs = self.get_object(pk)
         qs.delete()
         return ApiResponse(status=status.HTTP_204_NO_CONTENT)
