d18f438c13151ec9a68383d4292776793158a53d
shuvam021 <career.shuvam@gmail.com>
2022-03-12 13:58:38 +0530

    [Shuvam] Refactor: user's view, model, utils, serializer files

--- users/models.py
+++ users/models.py
@@ -1,10 +1,8 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.core.mail import send_mail
 from django.db import models
 from django.db.models.signals import post_save
 
-from users.utils import gen_token
+from users.utils import gen_token, MailService
 
 
 # Create your models here.
@@ -25,11 +23,7 @@ def user_post_save(sender, instance, created, *args, **kwargs):
     if created:
         try:
             token = gen_token({'id': instance.id})
-            msg_sub = "Verify Your Account"
-            msg_body = f"Hii {instance.email}\n"
-            msg_body += f"Click this link to verify your account\n"
-            msg_body += f"http://{settings.DOMAIN_NAME}/verify/{token}/"
-            send_mail(msg_sub, msg_body, settings.EMAIL_HOST, [instance.email], fail_silently=False)
+            MailService.verify_mail(email=instance.email, token=token)
         except Exception as e:
             print(e)
 
--- users/utils.py
+++ users/utils.py
@@ -1,21 +1,14 @@
 import jwt
 from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework.response import Response
 
 
 def ApiResponse(data=None, status=None, msg=""):
-    message_switcher = {
-        200: "Data fetch successful",
-        201: "Create or Update successful",
-        204: "Delete successful",
-        404: "User doesn't exist"
-    }
+    message_switcher = {200: "Data fetch successful", 201: "Create or Update successful", 204: "Delete successful",
+        404: "User doesn't exist"}
     status_list = list(message_switcher.keys())
-    mod_data = {
-        'status': status,
-        'message': message_switcher.get(status) if len(msg) == 0 else msg,
-        'data': data
-    }
+    mod_data = {'status': status, 'message': message_switcher.get(status) if len(msg) == 0 else msg, 'data': data}
     return Response(data=mod_data, status=status)
 
 
@@ -25,3 +18,21 @@ def gen_token(payload):
 
 def decode_token(token):
     return jwt.decode(token, settings.JWT_KEY, algorithms=settings.JWT_ALGORITHMS)
+
+
+class MailService:
+    @staticmethod
+    def verify_mail(email, token):
+        msg_sub = "Verify Your Account"
+        msg_body = f"Hii {email}\n"
+        msg_body += f"Click this link to verify your account\n"
+        msg_body += f"http://{settings.DOMAIN_NAME}/verify/{token}/"
+        send_mail(msg_sub, msg_body, settings.EMAIL_HOST, [email], fail_silently=False)
+
+    @staticmethod
+    def forget_password_mail(email, token):
+        msg_sub = "Change your password"
+        msg_body = f"Hii, {email}\n"
+        msg_body += f"use this link to change your password\n"
+        msg_body += f"http://{settings.DOMAIN_NAME}/change_password/{token}/"
+        send_mail(msg_sub, msg_body, settings.EMAIL_HOST, [email], fail_silently=False)
